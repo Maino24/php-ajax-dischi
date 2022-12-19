af42f59b58b97c51f7268d07df0572456c4a627c
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-19 19:08:54 +0100

    main

--- versione due/parziali/body.php
+++ versione due/parziali/body.php
@@ -9,7 +9,16 @@
 
     <div class="container">
         <div class="row p-b-4">
-            <!--- qui ci andrà la card -->
+        
+        <div class="col-4 g-3" v-for="(elem, index) in arrayDischi" :key="index">
+            <div class="card" style="width: 18rem;">
+                <img :src="elem.poster" class="card-img-top" :alt="elem.title">
+                <div class="card-body">
+                    <h5 class="card-title">{{elem.title}}</h5>
+                    <p class="card-text">{{elem.genre}}</p>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
 
